db4b5149fc248a8f71a053ef53ebfee393cbedd2
ShubhamBadadale <shubhambadadale@gmail.com>
2026-06-19 15:18:40 +0530

    refactor: extract business logic to TaskService

--- src/main/java/com/Shubham/task_manager/TaskController.java
+++ src/main/java/com/Shubham/task_manager/TaskController.java
@@ -1,5 +1,5 @@
 package com.Shubham.task_manager;
-
+import com.Shubham.task_manager.service.TaskService;
 import com.Shubham.task_manager.model.Task;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,22 +11,25 @@ import java.util.List;
 
 @RestController
 public class TaskController {
-    private List<Task> tasks = new ArrayList<>();
+    private final TaskService taskService;
+    public TaskController(TaskService taskService){
+        this.taskService = taskService;
+    }
+//    private List<Task> tasks = new ArrayList<>();
     @GetMapping("/health")
     public String healthCheck(){
         return "The Task Manager API is running!";
     }
     @GetMapping("/task")
     public List<Task> taskCheck(){
-//        Task t1 = new Task(10l , "Title1" , true);
-//        tasks.add(t1);
-        return tasks;
+//
+
+        return taskService.getAllTasks();;
     }
     @PostMapping("/task")
     public Task createTask(@RequestBody Task newTask){
-//        Task t1 = new Task(10l , "Title1" , true);
-        tasks.add(newTask);
-        return newTask;
+//
+        return taskService.createTask(newTask);
     }
 
 
